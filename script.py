3b6093fff0037d4e04b2134a7c29411203503652
Etheryen <lukasz.ciwoniuk@gmail.com>
2019-02-03 12:24:29 +0100

    First commit

---
+++ script.py
@@ -0,0 +1,14 @@
+﻿# 25.01.2019
+# version 1.0
+
+import ctypes
+import datetime
+import sys
+from pathlib import Path
+
+def wallpaper(numer):
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, str(Path(("PNG/{}.png").format(numer)).resolve()), 0)
+
+wallpaper(datetime.datetime.today().weekday() + 1)
+
+sys.exit()
